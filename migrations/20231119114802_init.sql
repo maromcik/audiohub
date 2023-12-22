09e492167007c55624579ec1ed394e6fc25cb9d7
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2023-12-22 22:09:30 +0100

    fix(db) - added default DEFAULT make_interval(0,0,0,0,0,0,0),

--- migrations/20231119114802_init.sql
+++ migrations/20231119114802_init.sql
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS "Active_Audiobook"
     user_id                         bigserial        NOT NULL,
     audiobook_id                    bigserial        NOT NULL,
     playback_chapter_id             bigserial,
-    playback_position_in_chapter    interval,
+    playback_position_in_chapter    interval        DEFAULT make_interval(0,0,0,0,0,0,0),
 
     PRIMARY KEY (user_id, audiobook_id, playback_chapter_id),
     FOREIGN KEY (user_id)       REFERENCES "User" (id),
