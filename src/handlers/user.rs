a3b6bfaa344a1c182c2542a32140787a15396795
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-10 22:00:59 +0100

    wip(user): dependency injection does not work, I tried to get rid of the Arc that wraps around PgPool with no success

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,14 +1,16 @@
-use actix_web::{get, HttpResponse, web};
-use actix_web::error::ErrorInternalServerError;
+use crate::database::common::error::{BusinessLogicErrorKind, DbError, DbResultSingle};
 use crate::database::common::DbCreate;
 use crate::database::models::user::UserCreate;
 use crate::database::repositories::user::repository::UserRepository;
 use crate::error::AppError;
+use actix_web::error::ErrorInternalServerError;
+use actix_web::{get, web, HttpResponse};
 
 #[get("/index")]
-pub async fn index(mut user_repo: web::Data<UserRepository>) -> Result<HttpResponse, AppError> {
+pub async fn index(user_repo: web::Data<UserRepository>) -> Result<HttpResponse, AppError> {
     // let template = crate::templates::user::RegistrationTemplate {};
     // let body = template.render().map_err(ErrorInternalServerError)?;
-    let b = user_repo.create(&UserCreate::new("", "", "", "", "", "", "", "")).await?;
+    // db is broken, I dont have time rn.
+    // let b = user_repo.create(&UserCreate::new("", "", "", "", "", "", "", "")).await?;
     Ok(HttpResponse::Ok().finish())
 }
