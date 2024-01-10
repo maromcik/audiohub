f9646d47cd97cc3dd2d65059ee9b0b7ecbfec076
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-10 23:01:29 +0100

    feat(actix example): conventions

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,3 +1,4 @@
+use crate::database::common::error::{BusinessLogicErrorKind, DbResultMultiple};
 use crate::database::common::{DbDelete, DbReadOne};
 use crate::database::models::user::{UserDelete, UserLogin};
 use crate::database::repositories::user::repository::UserRepository;
@@ -12,5 +13,34 @@ pub async fn index(user_repo: web::Data<UserRepository>) -> Result<HttpResponse,
     let body = template.render()?;
     let _user1 = user_repo.read_one(&UserLogin::new("", "")).await?;
     let _user2 = user_repo.delete(&UserDelete::new(&100)).await;
+
+    // match _user2 {
+    //     Ok(_) => {Ok(HttpResponse::Ok().content_type("text/html").body(body))}
+    //     Err(e) => {
+    //         match e.business_error.error_kind {
+    //             BusinessLogicErrorKind::UserDoesNotExist => {}
+    //             BusinessLogicErrorKind::UserDeleted => {}
+    //             BusinessLogicErrorKind::UserPasswordDoesNotMatch => {}
+    //             BusinessLogicErrorKind::UserUpdateParametersEmpty => {}
+    //             BusinessLogicErrorKind::AudiobookDoesNotExist => {}
+    //             BusinessLogicErrorKind::AudiobookDeleted => {}
+    //             BusinessLogicErrorKind::AudiobookUpdateParametersEmpty => {}
+    //             BusinessLogicErrorKind::RatingDoesNotExist => {}
+    //             BusinessLogicErrorKind::RatingDeleted => {}
+    //             BusinessLogicErrorKind::RatingUpdateParametersEmpty => {}
+    //             BusinessLogicErrorKind::ChapterDoesNotExist => {}
+    //             BusinessLogicErrorKind::ChapterDeleted => {}
+    //             BusinessLogicErrorKind::ChapterUpdateParametersEmpty => {}
+    //             BusinessLogicErrorKind::GenreDeleted => {}
+    //             BusinessLogicErrorKind::GenreDoesNotExist => {}
+    //             BusinessLogicErrorKind::GenreUpdateParametersEmpty => {}
+    //             BusinessLogicErrorKind::DatabaseError => {}
+    //             BusinessLogicErrorKind::MigrationError => {}
+    //             BusinessLogicErrorKind::UniqueConstraintError => {}
+    //             BusinessLogicErrorKind::NotNullError => {}
+    //             BusinessLogicErrorKind::ForeignKeyError => {}
+    //         }
+    //     }
+    // }
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
