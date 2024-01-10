8dc6ab5aad5d7f7e679343f5be9cf619debbc5fb
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-10 19:59:22 +0100

    fix(handlers): removed unused imports

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,15 +1,12 @@
 // handlers.rs
-use crate::database::common::DbReadOne;
-use crate::database::models::user::{UserCreate, UserLogin};
 use crate::database::repositories::user::repository::UserRepository;
 use crate::templates::{LoginTemplate, RegistrationTemplate};
-use actix_files::{Files as ActixFiles, Files};
+
 use actix_web::{
-    delete, error::ErrorInternalServerError, get, patch, post, put, web, App, HttpResponse,
-    HttpServer, Result as ActixResult,
+    delete, error::ErrorInternalServerError, get, patch, post, put, web, HttpResponse, Result as ActixResult,
 };
 use askama::Template;
-use sqlx::PgPool; // Assume UserForm is a struct representing the form input
+
 
 #[get("/register")]
 pub async fn register(user_repo: web::Data<UserRepository>) -> ActixResult<HttpResponse> {
