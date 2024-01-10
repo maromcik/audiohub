2587af16fb0d0a2e1f688fe2a4d82fc14eba368e
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-10 19:58:36 +0100

    fix(paths): still with an error

--- src/init.rs
+++ src/init.rs
@@ -39,6 +39,6 @@ pub fn configure_webapp(pool: &Arc<PgPool>) -> Box<dyn FnOnce(&mut ServiceConfig
             .service(audiobook_scope)
             .service(chapter_scope)
             .service(rating_scope)
-            .service(ActixFiles::new("/", "./src/static").prefer_utf8(true));
+            .service(ActixFiles::new("/", "./static").prefer_utf8(true));
     })
 }
