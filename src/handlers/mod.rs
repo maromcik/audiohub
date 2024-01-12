86b45ec42782ddcfd1f75b2bf744b5c3921cdfe3
Nina Rybarova <xrybaro1@fi.muni.cz>
2024-01-12 12:10:27 +0100

    fixed register

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,7 @@
 pub mod homepage;
 pub mod user;
 
-pub use crate::handlers::user::register_user as create_user;
+pub use crate::handlers::user::register as create_user;
 pub use crate::handlers::user::login as user_login_page;
 pub use crate::handlers::user::login_user as login_user;
 pub use crate::handlers::user::register_user as user_register_page;
