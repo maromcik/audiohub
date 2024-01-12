a9229750cdc03fe86e0af99e07bda4c47aa7fa16
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-12 12:14:47 +0100

    fix(user): coherent naming

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,9 +1,9 @@
 pub mod homepage;
 pub mod user;
 
-pub use crate::handlers::user::register as create_user;
+pub use crate::handlers::user::register as user_register_page;
 pub use crate::handlers::user::login as user_login_page;
-pub use crate::handlers::user::login_user as login_user;
-pub use crate::handlers::user::register_user as user_register_page;
+pub use crate::handlers::user::login_user as user_login;
+pub use crate::handlers::user::register_user as user_register;
 
 pub use crate::handlers::homepage::homepage as index;
