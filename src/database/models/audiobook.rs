b9d8dd8dbaf84e04d9fae8b3035fd09cb22e4aa9
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-15 17:33:40 +0100

    feat(genre): implemented search for genres

--- src/database/models/audiobook.rs
+++ src/database/models/audiobook.rs
@@ -73,9 +73,9 @@ impl AudiobookSearch {
     pub fn new(
         name: Option<&str>,
         author_id: Option<Id>,
-        _author_name: Option<&str>,
+        author_name: Option<&str>,
         genre_id: Option<Id>,
-        _genre_name: Option<&str>,
+        genre_name: Option<&str>,
         min_length: Option<PgInterval>,
         max_length: Option<PgInterval>,
         min_stream_count: Option<i64>,
@@ -87,8 +87,8 @@ impl AudiobookSearch {
     ) -> Self {
         Self {
             name: name.map(|n| n.to_owned()),
-            genre_name: name.map(|n| n.to_owned()),
-            author_name: name.map(|n| n.to_owned()),
+            genre_name: genre_name.map(|n| n.to_owned()),
+            author_name: author_name.map(|n| n.to_owned()),
             author_id: author_id.map(|n| n.to_owned()),
             genre_id: genre_id.map(|n| n.to_owned()),
             min_length: min_length.map(|n| n.to_owned()),
@@ -101,134 +101,150 @@ impl AudiobookSearch {
             max_overall_rating: max_overall_rating.map(|n| n.to_owned()),
         }
     }
-}
-
-#[derive(Debug, Clone)]
-pub struct AudiobookCreate {
-    pub name: String,
-    pub author_id: Id,
-    pub genre_id: Id,
-    pub length: PgInterval,
-    pub file_path: String,
-    pub thumbnail: String,
-    pub description: String,
-}
-
-impl AudiobookCreate {
-    #[must_use]
-    #[inline]
-    #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        name: &str,
-        author_id: &Id,
-        genre_id: &Id,
-        length: &PgInterval,
-        file_path: &str,
-        thumbnail: &str,
-        description: &str,
-    ) -> Self {
+    pub fn search_by_genre_id(genre_id: Option<Id>) -> Self {
         Self {
-            name: name.to_owned(),
-            author_id: *author_id,
-            genre_id: *genre_id,
-            length: length.clone(),
-            file_path: file_path.to_owned(),
-            thumbnail: thumbnail.to_owned(),
-            description: description.to_owned(),
-        }
-    }
-}
-
-pub struct AudiobookUpdate {
-    pub id: Id,
-    pub name: Option<String>,
-    pub author_id: Option<Id>,
-    pub genre_id: Option<Id>,
-    pub length: Option<PgInterval>,
-    pub file_path: Option<String>,
-    pub stream_count: Option<i64>,
-    pub like_count: Option<i64>,
-    pub overall_rating: Option<i16>,
-    pub thumbnail: Option<String>,
-    pub description: Option<String>,
-}
-
-impl AudiobookUpdate {
-    #[inline]
-    #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        id: &Id,
-        name: Option<&str>,
-        author_id: Option<&Id>,
-        genre_id: Option<&Id>,
-        length: Option<&PgInterval>,
-        file_path: Option<&str>,
-        stream_count: Option<&i64>,
-        like_count: Option<&i64>,
-        overall_rating: Option<&i16>,
-        thumbnail: Option<&str>,
-        description: Option<&str>,
-    ) -> Self {
-        let change_to_owned = |value: &str| Some(value.to_owned());
-        Self {
-            id: *id,
-            name: name.and_then(change_to_owned),
-            author_id: author_id.copied(),
-            genre_id: genre_id.copied(),
-            length: length.cloned(),
-            file_path: file_path.and_then(change_to_owned),
-            stream_count: stream_count.copied(),
-            like_count: like_count.copied(),
-            overall_rating: overall_rating.copied(),
-            thumbnail: thumbnail.and_then(change_to_owned),
-            description: description.and_then(change_to_owned),
+            name: None,
+            genre_name: None,
+            author_name: None,
+            author_id: None,
+            genre_id: genre_id.map(|n| n.to_owned()),
+            min_length: None,
+            max_length: None,
+            min_stream_count: None,
+            max_stream_count: None,
+            min_like_count: None,
+            max_like_count: None,
+            min_overall_rating: None,
+            max_overall_rating: None,
         }
     }
 
-    #[must_use]
-    pub const fn update_fields_none(&self) -> bool {
-        self.name.is_none()
-            && self.author_id.is_none()
-            && self.genre_id.is_none()
-            && self.length.is_none()
-            && self.file_path.is_none()
-            && self.stream_count.is_none()
-            && self.like_count.is_none()
-            && self.overall_rating.is_none()
-            && self.description.is_none()
-            && self.thumbnail.is_none()
+    #[derive(Debug, Clone)]
+    pub struct AudiobookCreate {
+        pub name: String,
+        pub author_id: Id,
+        pub genre_id: Id,
+        pub length: PgInterval,
+        pub file_path: String,
+        pub thumbnail: String,
+        pub description: String,
     }
-}
 
-#[derive(Debug, Clone)]
-pub struct AudiobookDelete {
-    pub id: Id,
-}
-
-impl AudiobookDelete {
-    #[must_use]
-    #[inline]
-    pub fn new(id: &Id) -> Self {
-        Self { id: *id }
+    impl AudiobookCreate {
+        #[must_use]
+        #[inline]
+        #[allow(clippy::too_many_arguments)]
+        pub fn new(
+            name: &str,
+            author_id: &Id,
+            genre_id: &Id,
+            length: &PgInterval,
+            file_path: &str,
+            thumbnail: &str,
+            description: &str,
+        ) -> Self {
+            Self {
+                name: name.to_owned(),
+                author_id: *author_id,
+                genre_id: *genre_id,
+                length: length.clone(),
+                file_path: file_path.to_owned(),
+                thumbnail: thumbnail.to_owned(),
+                description: description.to_owned(),
+            }
+        }
     }
-}
 
-#[derive(Deserialize, Debug, Clone)]
-pub struct AudiobookGetById {
-    pub id: Id,
-}
-
-impl AudiobookGetById {
-    #[must_use]
-    #[inline]
-    pub const fn new(id: &Id) -> Self {
-        Self { id: *id }
+    pub struct AudiobookUpdate {
+        pub id: Id,
+        pub name: Option<String>,
+        pub author_id: Option<Id>,
+        pub genre_id: Option<Id>,
+        pub length: Option<PgInterval>,
+        pub file_path: Option<String>,
+        pub stream_count: Option<i64>,
+        pub like_count: Option<i64>,
+        pub overall_rating: Option<i16>,
+        pub thumbnail: Option<String>,
+        pub description: Option<String>,
     }
-}
 
-#[derive(Debug, Clone)]
-pub struct AudiobookMetadataForm {
-    pub name: String,
-    pub description: String,
-    pub genre_id: Id,
-}
+    impl AudiobookUpdate {
+        #[inline]
+        #[allow(clippy::too_many_arguments)]
+        pub fn new(
+            id: &Id,
+            name: Option<&str>,
+            author_id: Option<&Id>,
+            genre_id: Option<&Id>,
+            length: Option<&PgInterval>,
+            file_path: Option<&str>,
+            stream_count: Option<&i64>,
+            like_count: Option<&i64>,
+            overall_rating: Option<&i16>,
+            thumbnail: Option<&str>,
+            description: Option<&str>,
+        ) -> Self {
+            let change_to_owned = |value: &str| Some(value.to_owned());
+            Self {
+                id: *id,
+                name: name.and_then(change_to_owned),
+                author_id: author_id.copied(),
+                genre_id: genre_id.copied(),
+                length: length.cloned(),
+                file_path: file_path.and_then(change_to_owned),
+                stream_count: stream_count.copied(),
+                like_count: like_count.copied(),
+                overall_rating: overall_rating.copied(),
+                thumbnail: thumbnail.and_then(change_to_owned),
+                description: description.and_then(change_to_owned),
+            }
+        }
+
+        #[must_use]
+        pub const fn update_fields_none(&self) -> bool {
+            self.name.is_none()
+                && self.author_id.is_none()
+                && self.genre_id.is_none()
+                && self.length.is_none()
+                && self.file_path.is_none()
+                && self.stream_count.is_none()
+                && self.like_count.is_none()
+                && self.overall_rating.is_none()
+                && self.description.is_none()
+                && self.thumbnail.is_none()
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct AudiobookDelete {
+        pub id: Id,
+    }
+
+    impl AudiobookDelete {
+        #[must_use]
+        #[inline]
+        pub fn new(id: &Id) -> Self {
+            Self { id: *id }
+        }
+    }
+
+    #[derive(Deserialize, Debug, Clone)]
+    pub struct AudiobookGetById {
+        pub id: Id,
+    }
+
+    impl AudiobookGetById {
+        #[must_use]
+        #[inline]
+        pub const fn new(id: &Id) -> Self {
+            Self { id: *id }
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct AudiobookMetadataForm {
+        pub name: String,
+        pub description: String,
+        pub genre_id: Id,
+    }
--- src/handlers/genre.rs
+++ src/handlers/genre.rs
@@ -5,18 +5,17 @@ use crate::database::repositories::genre::repository::GenreRepository;
 use crate::error::AppError;
 use crate::templates::genre::AllGenresTemplate;
 use askama::Template;
-use sqlx::postgres::types::PgInterval;
 use crate::database::models::audiobook::AudiobookSearch;
 use crate::database::models::Id;
 use crate::database::repositories::audiobook::repository::AudiobookRepository;
-use crate::templates::audiobook::{AudiobooksByGenreTemplate, NewReleasesTemplate};
+use crate::templates::audiobook::AudiobooksByGenreTemplate;
 
 #[get("/")]
 async fn get_genres(genre_repo: web::Data<GenreRepository>) -> Result<HttpResponse, AppError> {
     //get all genres
     let genres = genre_repo.read_many(&GenreSearch::new(None)).await?;
 
-    let template = AllGenresTemplate { genres: genres };
+    let template = AllGenresTemplate { genres };
     let body = template.render()?;
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
@@ -27,15 +26,7 @@ async fn get_audiobooks_by_genre(
     genre_repo: web::Data<GenreRepository>,
     path: web::Path<(Id,)>) -> Result<HttpResponse, AppError> {
     let genre_id = path.into_inner().0;
-    let book_search = AudiobookSearch{
-        name : None, author_name: None,
-        genre_name: None, author_id: None,
-        genre_id: Some(genre_id), min_length: None,
-        max_length: None, min_stream_count: None,
-        max_stream_count: None, min_like_count: None,
-        max_like_count: None, min_overall_rating: None,
-        max_overall_rating: None
-    };
+    let book_search = AudiobookSearch::search_by_genre_id(Some(genre_id));
     let books = audiobook_repo
         .read_many(&book_search)
         .await?;
