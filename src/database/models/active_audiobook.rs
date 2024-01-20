9e8fc4b8d062b249ee0c6a1cd99a88fc1700c8c2
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-20 11:07:27 +0100

    fix(audiobook): added is_finished, is_active and is_never_started methods, filtering by percentage

--- src/database/models/active_audiobook.rs
+++ src/database/models/active_audiobook.rs
@@ -1,5 +1,6 @@
 use crate::database::models::Id;
 use chrono::{DateTime, Utc};
+use crate::CONSIDER_AUDIOBOOK_FINISHED_PERCENTAGE;
 use crate::database::models::audiobook::AudiobookDetail;
 
 #[derive(sqlx::FromRow, Debug, PartialEq, Clone)]
@@ -61,6 +62,43 @@ impl SetActiveAudiobook {
 }
 
 #[derive(sqlx::FromRow, Debug, Clone, PartialEq)]
+pub struct ActiveAudiobookJoin {
+    pub id: Id,
+    // --------------
+    pub name: String,
+    pub author_id: Id,
+    pub genre_id: Id,
+    pub file_path: String,
+    pub length: f64,
+    pub stream_count: i64,
+    pub like_count: i64,
+    pub overall_rating: i16,
+    pub thumbnail: String,
+    pub description: String,
+    pub created_at: DateTime<Utc>,
+    pub edited_at: DateTime<Utc>,
+
+    pub username: String,
+    pub email: String,
+    pub author_name: String,
+    pub surname: String,
+    pub bio: String,
+    pub profile_picture: String,
+
+    pub genre_name: String,
+
+    pub playback_position: f64,
+    pub active_audiobook_edited_at: DateTime<Utc>,
+}
+
+impl ActiveAudiobookJoin {
+    pub fn is_finished(&self) -> bool {
+        self.playback_position / self.length * 100f64 > CONSIDER_AUDIOBOOK_FINISHED_PERCENTAGE
+    }
+}
+
+
+#[derive(sqlx::FromRow, Debug, Clone, PartialEq)]
 pub struct ActiveAudiobookDetail {
     pub id: Id,
     // --------------
@@ -119,6 +157,40 @@ impl ActiveAudiobookDetail {
             playback_position,
             active_audiobook_edited_at,
             progress: playback_position / audiobook.length * 100f64
+        }
+    }
+    pub fn is_finished(&self) -> bool {
+        self.playback_position / self.length * 100f64 > CONSIDER_AUDIOBOOK_FINISHED_PERCENTAGE
+    }
+}
+
+impl From<ActiveAudiobookJoin> for ActiveAudiobookDetail {
+    fn from(audiobook: ActiveAudiobookJoin) -> Self {
+        Self {
+            id: audiobook.id,
+            name: audiobook.name.to_owned(),
+            author_id: audiobook.author_id,
+            genre_id: audiobook.genre_id,
+            file_path: audiobook.file_path.to_owned(),
+            length: audiobook.length,
+            thumbnail: audiobook.thumbnail.to_owned(),
+            description: audiobook.description.to_owned(),
+            stream_count: audiobook.stream_count,
+            like_count: audiobook.like_count,
+            overall_rating: audiobook.overall_rating,
+            created_at: audiobook.created_at,
+            edited_at: audiobook.edited_at,
+
+            username: audiobook.username.to_owned(),
+            email: audiobook.email.to_owned(),
+            author_name: audiobook.author_name.to_owned(),
+            surname: audiobook.surname.to_owned(),
+            bio: audiobook.bio.to_owned(),
+            profile_picture: audiobook.profile_picture.to_owned(),
+            genre_name: audiobook.genre_name.to_owned(),
+            playback_position: audiobook.playback_position,
+            active_audiobook_edited_at: audiobook.active_audiobook_edited_at,
+            progress: audiobook.playback_position / audiobook.length * 100f64
         }
     }
 }
