2e7468886bcae843e81eab8ac881a2db55b43b50
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-22 12:05:28 +0100

    feat(chapters): added the remove handler + modified db query

--- src/database/repositories/chapter/repository.rs
+++ src/database/repositories/chapter/repository.rs
@@ -1,12 +1,12 @@
 use crate::database::common::error::BackendErrorKind::{
     ChapterDeleted, ChapterDoesNotExist, RatingUpdateParametersEmpty,
 };
-use crate::database::common::error::{BackendError, DbError, DbResultMultiple, DbResultSingle};
+use crate::database::common::error::{BackendError, BackendErrorKind, DbError, DbResultMultiple, DbResultSingle};
 use crate::database::common::{
     DbCreate, DbDelete, DbPoolHandler, DbReadMany, DbReadOne, DbRepository, DbUpdate, PoolHandler,
 };
 use crate::database::models::audiobook::AudiobookGetById;
-use crate::database::models::chapter::{Chapter, ChapterCreate, ChapterGetById, ChapterSearch, ChapterUpdate, ChaptersGetByBookId, ChapterDisplay, ChapterDetail, ChaptersGetByBookIdJoin};
+use crate::database::models::chapter::{Chapter, ChapterCreate, ChapterGetById, ChapterSearch, ChapterUpdate, ChaptersGetByBookId, ChapterDisplay, ChapterDetail, ChaptersGetByBookIdJoin, ChapterRemoveById};
 use async_trait::async_trait;
 use sqlx::{Postgres, Transaction};
 
@@ -245,15 +245,40 @@ impl DbReadMany<ChaptersGetByBookIdJoin, ChapterDetail> for ChapterRepository {
 }
 
 #[async_trait]
-impl DbDelete<ChapterGetById, Chapter> for ChapterRepository {
-    async fn delete(&self, params: &ChapterGetById) -> DbResultMultiple<Chapter> {
+impl DbDelete<ChapterRemoveById, Chapter> for ChapterRepository {
+    async fn delete(&self, params: &ChapterRemoveById) -> DbResultMultiple<Chapter> {
         let mut transaction = self.pool_handler.pool.begin().await?;
-        let chapter = ChapterRepository::get(params, &mut transaction).await?;
-        ChapterRepository::is_correct(chapter)?;
+        let chapter =  sqlx::query_as!(
+            ChapterDetail,
+            r#"
+            SELECT
+                c.id,
+                c.name,
+                c.audiobook_id,
+                c.position,
+                c.created_at,
+                c.edited_at,
+                c.deleted_at,
+                a.name AS audiobook_name,
+                a.author_id
+            FROM
+                "Chapter" AS c
+                    INNER JOIN
+                "Audiobook" AS a ON c.audiobook_id = a.id
+            WHERE
+                c.deleted_at IS NULL
+                AND c.id = $1
+            "#,
+            params.id,
+        )
+            .fetch_one(transaction.as_mut())
+            .await?;
 
-        let chapter = ChapterRepository::delete_chapter(params, &mut transaction).await?;
+        if params.user_id != chapter.author_id {
+            return Err(DbError::from(BackendError::new(BackendErrorKind::UnauthorizedOperation)));
+        }
+        let chapter = ChapterRepository::delete_chapter(&ChapterGetById::new(params.id), &mut transaction).await?;
         transaction.commit().await?;
-
         Ok(vec![chapter])
     }
 }
--- src/handlers/chapter.rs
+++ src/handlers/chapter.rs
@@ -1,19 +1,20 @@
 use crate::authorized;
-use crate::database::common::{DbCreate, DbReadMany, DbReadOne};
-use crate::database::models::chapter::{ChapterCreate, ChapterGetById, ChaptersGetByBookId};
+use crate::database::common::{DbCreate, DbDelete, DbReadMany, DbReadOne};
+use crate::database::models::chapter::{ChapterCreate, ChapterGetById, ChapterRemoveById, ChaptersGetByBookId};
 
 use crate::database::repositories::chapter::repository::ChapterRepository;
 use crate::error::AppError;
-use crate::forms::chapter::ChapterCreateForm;
+use crate::forms::chapter::{ChapterCreateForm, ChapterDeleteForm};
 use crate::templates::chapter::{ChapterCreatorPlayerTemplate, ChapterDetailTemplate, ChapterListTemplate, ChapterTimelineTemplate};
 use actix_identity::Identity;
 use actix_web::http::header::LOCATION;
-use actix_web::{post, get, web, HttpResponse, App};
+use actix_web::{post, get, web, HttpResponse, App, delete};
 use askama::Template;
 use crate::database::models::audiobook::{Audiobook, AudiobookGetById};
 use crate::database::models::Id;
 use crate::database::repositories::audiobook::repository::AudiobookRepository;
 use crate::handlers::helpers::transform_to_displayable_chapters;
+use crate::handlers::utilities::parse_user_id;
 
 
 #[post("/create")]
@@ -41,13 +42,13 @@ pub async fn create_chapter(
 pub async fn audio_selection_for_chapter(
     identity: Option<Identity>,
     path: web::Path<Id>,
-    audiobook_repo: web::Data<AudiobookRepository>
+    audiobook_repo: web::Data<AudiobookRepository>,
 ) -> Result<HttpResponse, AppError> {
     authorized!(identity);
-    let book = audiobook_repo.read_one(&AudiobookGetById{id: path.into_inner()})
+    let book = audiobook_repo.read_one(&AudiobookGetById { id: path.into_inner() })
         .await?;
 
-    let template = ChapterCreatorPlayerTemplate {source: book.file_path};
+    let template = ChapterCreatorPlayerTemplate { source: book.file_path };
     Ok(HttpResponse::Ok().content_type("text/html").body(template.render()?))
 }
 
@@ -60,10 +61,10 @@ pub async fn get_chapter_timeline(
     authorized!(identity);
 
     let book_id = path.into_inner();
-    let chapters = chapter_repo.read_many(&ChaptersGetByBookId {audiobook_id: book_id}).await?;
-    let book = audiobook_repo.read_one(&AudiobookGetById{id: book_id}).await?;
+    let chapters = chapter_repo.read_many(&ChaptersGetByBookId { audiobook_id: book_id }).await?;
+    let book = audiobook_repo.read_one(&AudiobookGetById { id: book_id }).await?;
     let displayable_chapters = transform_to_displayable_chapters(chapters);
-    let template = ChapterTimelineTemplate {book_id, chapters: displayable_chapters, length: book.length};
+    let template = ChapterTimelineTemplate { book_id, chapters: displayable_chapters, length: book.length };
     Ok(HttpResponse::Ok().content_type("text/html").body(template.render()?))
 }
 
@@ -72,14 +73,28 @@ pub async fn get_chapter_timeline(
 pub async fn get_chapter_list(
     identity: Option<Identity>,
     chapter_repo: web::Data<ChapterRepository>,
-    path: web::Path<Id>) -> Result<HttpResponse, AppError> {
+    path: web::Path<Id>)
+    -> Result<HttpResponse, AppError> {
     authorized!(identity);
 
     let book_id = path.into_inner();
-    let chapters = chapter_repo.read_many(&ChaptersGetByBookId {audiobook_id: book_id}).await?;
+    let chapters = chapter_repo.read_many(&ChaptersGetByBookId { audiobook_id: book_id }).await?;
     let displayable_chapters = transform_to_displayable_chapters(chapters);
 
-    let template = ChapterListTemplate {book_id, chapters: displayable_chapters};
+    let template = ChapterListTemplate { book_id, chapters: displayable_chapters };
     Ok(HttpResponse::Ok().content_type("text/html").body(template.render()?))
 }
 
+#[delete("/")]
+pub async fn remove_chapter(
+    identity: Option<Identity>,
+    chapter_repo: web::Data<ChapterRepository>,
+    form: web::Form<ChapterDeleteForm>,
+) -> Result<HttpResponse, AppError> {
+    let u = authorized!(identity);
+    chapter_repo.delete(&ChapterRemoveById::new(form.chapter_id, parse_user_id(u)?)).await?;
+    let chapters = chapter_repo.read_many(&ChaptersGetByBookId { audiobook_id: form.audiobook_id }).await?;
+    let displayable_chapters = transform_to_displayable_chapters(chapters);
+    let template = ChapterListTemplate { book_id: form.audiobook_id, chapters: displayable_chapters };
+    Ok(HttpResponse::Ok().content_type("text/html").body(template.render()?))
+}
