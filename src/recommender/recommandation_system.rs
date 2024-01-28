85c3b09ff1389bb1922ea81359308824091c7d6b
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-28 01:11:07 +0100

    fix(ai): url

--- src/recommender/recommandation_system.rs
+++ src/recommender/recommandation_system.rs
@@ -3,12 +3,14 @@ use crate::recommender_grpc_api::{
     AddBookRequest, BooksCollection, DeleteBookRequest, SimilarRequest,
 };
 
+const URL: &str = "https://audiohub-recommender.dyn.cloud.e-infra.cz";
+
 pub async fn init_recommendation_system(
     book_bios: Vec<&str>,
     book_ids: Vec<i64>,
     book_genres: Vec<&str>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ModelAiClient::connect("http://[::1]:50051").await?;
+    let mut client = ModelAiClient::connect(URL).await?;
 
     let request = tonic::Request::new(BooksCollection {
         bios: book_bios.iter().map(|x| x.to_string()).collect(),
@@ -25,7 +27,7 @@ pub async fn add_book_to_recommendation_system(
     book_id: i64,
     book_genre: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ModelAiClient::connect("http://[::1]:50051").await?;
+    let mut client = ModelAiClient::connect(URL).await?;
 
     let request = tonic::Request::new(AddBookRequest {
         bio: book_bio.to_string(),
@@ -40,7 +42,7 @@ pub async fn add_book_to_recommendation_system(
 pub async fn delete_book_from_recommendation(
     book_id: i64,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ModelAiClient::connect("http://[::1]:50051").await?;
+    let mut client = ModelAiClient::connect(URL).await?;
 
     let request = tonic::Request::new(DeleteBookRequest { id: book_id });
 
@@ -54,7 +56,7 @@ pub async fn recommend_books(
     book_genre: &str,
     count: i32,
 ) -> Result<Vec<i64>, Box<dyn std::error::Error>> {
-    let mut client = ModelAiClient::connect("http://[::1]:50051").await?;
+    let mut client = ModelAiClient::connect(URL).await?;
 
     let request = tonic::Request::new(SimilarRequest {
         id: book_id,
