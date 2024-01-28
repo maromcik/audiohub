ca2b65e8f1080640cc9ca6e1bddbc1c661511242
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-28 11:58:11 +0100

    fix(.sh): removed from git

--- kubernetes.sh
+++
@@ -1,5 +0,0 @@
-podman login cerit.io
-podman build -t cerit.io/roman_alexander_mariancik/audiohub-recommender-image ./recommender-server/.
-podman build -t cerit.io/roman_alexander_mariancik/audiohub-image .
-podman push cerit.io/roman_alexander_mariancik/audiohub-recommender-image:latest
-podman push cerit.io/roman_alexander_mariancik/audiohub-image:latest
